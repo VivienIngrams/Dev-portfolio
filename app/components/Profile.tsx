17ef56824d602d78c7a236ea48292ce2fcb9e91f
VivienIngrams <vivingrams@gmail.com>
2023-09-14 17:32:57 +0100

    Added styling to Navbar

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -7,9 +7,9 @@ interface ProfileProps {
 
 const Profile: FC<ProfileProps> = ({ heading, message }) => {
   return (
-    <main className="flex items-center justify-center h-screen mb-10 bg-center bg-cover custom-img-dark">
+    <main className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img-dark">
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-emerald-900/30 z-[2]" />
-      <div className=" p-5 ml-[-10rem] z-[2] mt-[-10rem] text-white font-pt">
+      <div className=" p-5 z-[2] mt-[-10rem] text-white font-pt">
         <h2 className="text-5xl font-bold font-moda">{heading}</h2>
         <p className="py-5 text-xl">{message}</p>
         <button className="px-8 py-2 border">Book</button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-import { Navbar } from "./components/Navbar";
-import Profile from "./components/Profile";
+import { Navbar } from "./components/Navbar"
+import Profile from "./components/Profile"
 
-export default function Home() {
-  return <Profile heading="Vivien Ingrams" message="Fronted Web Developper" />;
-}
+const Home: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Profile heading="Vivien Ingrams" message="Frontend Web Developer" />
+    </>
+  );
+};
+
+export default Home;
